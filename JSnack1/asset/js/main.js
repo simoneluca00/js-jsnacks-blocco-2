54c242cfecbdd51b68edfe01874771bb55cd81ae
simoneluca00 <si.zaino@libero.it>
2022-03-17 09:56:01 +0100

    "validazione" isNaN

--- JSnack1/asset/js/main.js
+++ JSnack1/asset/js/main.js
@@ -5,6 +5,8 @@ let risultato = document.getElementById("container_number");
 
 if ( numero % 2 === 0 ) {
     risultato.innerHTML = `<span style="font-weight: bold">${numero}</span> è un numero pari`;
-} else {
+} else if ( numero % 2 === 1 ) {
     risultato.innerHTML = `${numero} è un numero dispari quindi stampiamo <span style="font-weight: bold">${numero + 1}</span>`;
-}
+} else if ( isNaN(numero) ) {
+    alert("Inserisci soltanto valori numerici. Ricarica la pagina per riprovare.");
+};
