9c17a7b2971af802baa34b8099c7e549bb2f5953
simoneluca00 <si.zaino@libero.it>
2022-03-20 17:47:42 +0100

    JSnack2 modifiche

--- JSnack2/asset/js/main.js
+++ JSnack2/asset/js/main.js
@@ -1,16 +1,23 @@
 // Snack2
 // Generatore di “nomi cognomi” casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
-let nome = [ "Marta", "Cristian", "Simone", "Luisa", "Mario", "Giulia", "Michele", "Annalisa"];
-let cognome = [ "Rossi", "Zaino", "Esposito", "Icolari", "Molinari", "Miccoli", "Stasi", "Abbatiello"];
-let tavolo = ["ROCK", "POP", "JAZZ", "BLUES", "DANCE", "TECHNO", "HIP-HOP", "REGGAE"]
+const nome = [ "Marta", "Cristian", "Simone", "Luisa", "Mario", "Giulia", "Michele", "Annalisa"];
+const cognome = [ "Rossi", "Zaino", "Esposito", "Icolari", "Molinari", "Miccoli", "Stasi", "Abbatiello"];
+const tavolo = ["ROCK", "POP", "JAZZ", "BLUES", "DANCE", "TECHNO", "HIP-HOP", "REGGAE"];
 
-for ( i = 0; i < 50; i++ ){
+let numeroInvitati = 15;
+
+for ( i = 0; i < numeroInvitati; i++ ){
     let nomeRandom = nome[Math.floor(Math.random()* nome.length)];
     let cognomeRandom = cognome[Math.floor(Math.random()* cognome.length)];
     let tavoloRandom = tavolo[Math.floor(Math.random()* tavolo.length)];
+    let invitati = `${nomeRandom} ${cognomeRandom} `
 
-    document.getElementById("nome_cognome").innerHTML += `<li>${nomeRandom} ${cognomeRandom} (tavolo 
-        <span style="color: red">${tavoloRandom}</span>)
-        </li>`
+    document.getElementById("nome_cognome").innerHTML += 
+    `
+        <li>${invitati} (tavolo 
+            <span style="color: red">${tavoloRandom}</span>)
+        </li>
+    `
 }
 
+// come escludere i nomi/cognomi già utilizzati?
